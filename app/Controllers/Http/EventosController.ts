3a4746820aa9dd7ba763296c6caebf7a60102253
Rita Chen <ritazhen1@hotmail.com>
2022-03-22 00:01:04 -0300

    Homepage
    quase ok

--- app/Controllers/Http/EventosController.ts
+++ app/Controllers/Http/EventosController.ts
@@ -4,11 +4,33 @@ export default class EventosController {
     public eventos = {
         1: {
             id: 1,
-            nome: "Esportes"
+            nome: "Violino",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu scelerisque felis imperdiet proin fermentum leo vel. Ultrices tincidunt arcu non sodales neque. Risus at ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Purus sit amet volutpat consequat mauris. Commodo ullamcorper a lacus vestibulum sed arcu non odio. A iaculis at erat pellentesque adipiscing commodo elit.",
+            foto: "assets/images/evento-violino.jpg",
+            data: "30/02/2022",
+            participantes: 11,
+            organizador: "Ciclano D. Tal",
+            local: "Auditório"
         },
         2: {
             id: 2,
             nome: "Dança",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu scelerisque felis imperdiet proin fermentum leo vel. Ultrices tincidunt arcu non sodales neque. Risus at ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Purus sit amet volutpat consequat mauris. Commodo ullamcorper a lacus vestibulum sed arcu non odio. A iaculis at erat pellentesque adipiscing commodo elit.",
+            foto: "assets/images/evento-danca.jpg",
+            data: "30/03/2022",
+            participantes: 15,
+            organizador: "Ciclana D. Tal",
+            local: "Salão de dança"
+        },
+        3: {
+            id: 3,
+            nome: "Esportes",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu scelerisque felis imperdiet proin fermentum leo vel. Ultrices tincidunt arcu non sodales neque. Risus at ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Purus sit amet volutpat consequat mauris. Commodo ullamcorper a lacus vestibulum sed arcu non odio. A iaculis at erat pellentesque adipiscing commodo elit.",
+            foto: "assets/images/evento-esporte.jpg",
+            data: "30/04/2022",
+            participantes: 20,
+            organizador: "Fulana D. Tal",
+            local: "Quadra"
         }
     }
 
@@ -22,8 +44,10 @@ export default class EventosController {
         preferencias: this.eventos
     }
 
+    public eventoPrincipal = this.eventos[1];
+
     public async index({view} : HttpContextContract) {
-        return view.render('evento/home', { eventos : this.eventos, usuario : this.usuario })
+        return view.render('evento/home', { eventos : this.eventos, usuario : this.usuario, eventoPrincipal : this.eventoPrincipal })
     }
 
     public async criarEvento({view} : HttpContextContract) {
