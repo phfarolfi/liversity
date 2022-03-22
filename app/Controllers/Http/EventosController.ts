b526c173663fb4f9cb79cd692f67019d94cab5b3
Rita Chen <ritazhen1@hotmail.com>
2022-03-21 22:57:07 -0300

    Alguns cards iniciais da página home

--- app/Controllers/Http/EventosController.ts
+++ app/Controllers/Http/EventosController.ts
@@ -12,8 +12,18 @@ export default class EventosController {
         }
     }
 
+    public usuario = { 
+        foto: "assets/images/estudante.jpg", 
+        nome: "Fulano D. Tal", 
+        curso: "Ciência da Computação", 
+        campus: "Nova Iguaçu - IM", 
+        certificados: 32, 
+        eventosCriados: 11,
+        preferencias: this.eventos
+    }
+
     public async index({view} : HttpContextContract) {
-        return view.render('evento/home', { eventos : this.eventos })
+        return view.render('evento/home', { eventos : this.eventos, usuario : this.usuario })
     }
 
     public async criarEvento({view} : HttpContextContract) {
