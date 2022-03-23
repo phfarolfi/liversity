946c96d66e6b0881852223a907f8f8058737c71a
livictor1 <livictorlucas@gmail.com>
2022-03-22 22:02:39 -0300

    Resolução conflitos

--- app/Controllers/Http/MuralController.ts
+++ app/Controllers/Http/MuralController.ts
@@ -10,27 +10,27 @@ export default class MuralController {
         },
         2: {
             id: 2,
-            nome: "Aulas de alongamento e corrida",
+            nome: "Aulas de corrida e Yoga",
             foto: "images/mural/corrida.jpg", 
             participantes: 1212,
         },
         3: {
             id: 3,
-            nome: "Grupo de dança e ritmo",
+            nome: "Campeonatos de dança e aulas de Ritmo",
             foto: "images/mural/dança.jpg",
-            participantes: 568,
+            participantes: 501,
         },
         4: {
             id: 4,
-            nome: "Jogos/Aulas de Handebol",
-            foto: "images/mural/handebol.jpg",
+            nome: "Campeonatos de Tênis de Mesa",
+            foto: "images/mural/tenis de mesa.jpg",
             participantes: 568,
         },
         5: {
             id: 5,
-            nome: "Campeonato de Tênis de Mesa",
-            foto: "images/mural/tenis de mesa.jpg",
-            participantes: 568,
+            nome: "Jogos/Aulas de Handebol",
+            foto: "images/mural/handebol.jpg",
+            participantes: 588,
         }
     }
     public async Murais({view} : HttpContextContract) {
