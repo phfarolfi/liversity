5099cf775eeb15508925ec40375d32257b9f95e5
Rita Chen <ritazhen1@hotmail.com>
2022-04-06 10:54:25 -0300

    Migrations de algumas models

---
+++ database/migrations/1649252543135_genders.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Genders extends BaseSchema {
+  protected tableName = 'genders'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name').notNullable()
+      
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
