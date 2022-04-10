8088c233b1580db4d7d697ca56da436509e851f3
Rita Chen <ritazhen1@hotmail.com>
2022-04-10 12:11:04 -0300

    Commit de create event do Vitor

--- app/Controllers/Http/EventsController.ts
+++ app/Controllers/Http/EventsController.ts
@@ -1,4 +1,6 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+import Event from 'App/Models/Event'
 
 export default class EventsController {
     public events = {
@@ -119,6 +121,43 @@ export default class EventsController {
         5: {
             id: 5,
             name: "Jogos/Aulas de Handebol"
+        }
+    }
+
+    public async eventView({ auth, response, view } : HttpContextContract) {
+        await auth.use('web').check()
+        if(auth.use('web').isLoggedIn)
+            return response.redirect().toRoute('index')
+
+        return view.render('events/CreateEvent')
+    }
+
+    public async publishEvent({/*auth,*/ request, response, session} : HttpContextContract) {
+        const { name, eventDate, limitSubscriptionDate, description, linkCommunicationGroup, /*photo, document*/ } = request.all()
+
+        if(!name || !eventDate || !limitSubscriptionDate || !description || !linkCommunicationGroup /*|| !photo || !document*/) {
+            session.flashExcept(['signUp'])
+            session.flash({ errors: { signUp: 'Preencha todos os campos solicitados' } })
+            return response.redirect().toRoute('EventsController.eventView')
+        }
+
+        var eventAlreadyExist = await Event.findByOrFail('name', name);
+        if(name != null) {
+            session.flashExcept(['signUp'])
+            session.flash({ errors: { signUp: 'Já existe ' } })
+            return response.redirect().toRoute('AccountController.eventView')
+        }
+
+        try {
+            //var user = await User.findBy('email', auth.user!.email) caso coloquemos o organizador no banco como usuário
+            var eventCreated = await Event.create({ name: name, eventDate: eventDate, initialSubscriptionDate: eventDate, limitSubscriptionDate: limitSubscriptionDate, description: description, categoryId:3, local:'Campus', linkCommunicationGroup: linkCommunicationGroup, /*photo: photo, document:document,*/ campusId: 1, statusId:1 });
+            //await Student.create({ userId: userCreated.id, completedProfile: false })
+            response.redirect().toRoute('event.view')
+        } catch {
+            session.flashExcept(['signUp'])
+            session.flash({ errors: { signUp: 'Não foi possível realizar o cadastro do evento.' } })
+
+            return response.redirect().toRoute('AccountController.eventView')
         }
     }
 
